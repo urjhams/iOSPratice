7266ec41ac243a4dae29417934c69982c5d2e469
Quan <urjhams@gmail.com>
2018-11-03 12:08:53 +0100

    new init

---
+++ ParallaxEffect/ParallaxEffect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParallaxEffect
+//
+//  Created by Quân Đinh on 03.11.18.
+//  Copyright © 2018 urjhams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
